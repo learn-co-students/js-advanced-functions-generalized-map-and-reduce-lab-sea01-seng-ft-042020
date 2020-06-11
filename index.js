3725f56244307264db30db7a5e4dccab33758934
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-11 10:51:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(array, func) {
+    let output = []
+    for (const element of array) {
+        output.push(func(element))
+    }
+    return output
+}
+
+function reduce(array, func, start) {
+    let accumulator = start
+
+    // This feels janky, pretty sure this is not the right way to do it.....
+    let test = func(array[0], accumulator)
+    if (isNaN(test)) {
+        accumulator = 0
+    } else if (test === undefined || !test) {
+        accumulator = true
+    }
+    // end janky section
+
+    for (const element of array) {
+        accumulator = func(element, accumulator)
+    }
+    return accumulator
+}
